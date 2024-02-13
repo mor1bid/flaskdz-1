de410033bd7d9b1e5319846b291cc2c4523db47f
De <spark49942@gmail.com>
2024-02-13 06:24:39 +0300

    flaskdz1-3opa

--- dz.py
+++ dz.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
 
 prog = Flask(__name__)
 
 @prog.route('/')
 def wares():
     return render_template('index.html')
+
+@prog.route('/boots/')
+def boots():
+    return render_template('boots.html')
+
+@prog.route('/clothes/')
+def clothes():
+    return render_template('clothes.html')
+
+@prog.route('/curtco/')
+def curtco():
+    return render_template('curtcobain.html')
 
 if __name__=="__main__":
     prog.run()
